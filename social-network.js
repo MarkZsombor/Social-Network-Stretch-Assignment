a9819eb0945fb341692f7f95f3a6a1b5959fb0ab
Mark Zsombor <markszsombor@gmail.com>
2017-11-03 02:51:45 +0000

    completed assignment with all working functions

--- social-network.js
+++ social-network.js
@@ -49,8 +49,6 @@ function isFollowedBy(userId) {
   for (var entry in data) {
     for (var i = 0; i < data[entry].follows.length; i++) {
       if (data[entry].follows[i] === userId) {
-        // console.log(Object.keys(data)[entry]);
-        // console.log(entry);
         listOfFans.push(entry);
       }
     }
@@ -58,7 +56,6 @@ function isFollowedBy(userId) {
   return listOfFans;
 }
 
-// console.log(isFollowedBy("f04"));
 
 function followsAndFollowers() {
   for (var i in data) {
@@ -81,6 +78,8 @@ function hasFollowsTheMost() {
   console.log(biggestFollower[0] + " follows the most people, " + biggestFollower[1] + ".");
 }
 
+// TO DO add ability to recognize tie cases
+
 function isFollowedTheMost() {
   var mostFollowers = ['', 0];
   for (var i in data) {
@@ -89,8 +88,10 @@ function isFollowedTheMost() {
       mostFollowers[1] = isFollowedBy(i).length;
     }
   }
-  console.log(mostFollowers);
+  console.log(mostFollowers[0] + ' is followed by ' + mostFollowers[1] + ' people, the most of everyone.');
 }
+
+// TO DO add ability to recognize tie cases
 
 function howManyOldFollowers(userId) {
   var numberOfOldFollowers = 0;
@@ -115,18 +116,91 @@ function mostOldFollowers() {
   console.log(oldPeopleLoveTheMost[0] + ' has the most followers over 30 at ' + oldPeopleLoveTheMost[1]);
 }
 
+// TO DO add ability to recognize tie cases
+
+function howMany30PlusFollowed(userId) {
+  var number30PlusFollowed = 0;
+  var allFollowed = data[userId].follows;
+  for (var i = 0; i < allFollowed.length; i++) {
+    var specificFollowed = allFollowed[i];
+    if (data[specificFollowed].age > 30) {
+      number30PlusFollowed++;
+    }
+  }
+  return number30PlusFollowed;
+}
+
+
+function followsMostOldPeople() {
+  var likesOldPeople = ['', 0];
+  for (var i in data) {
+    if (howMany30PlusFollowed(i) > likesOldPeople[1]) {
+      likesOldPeople[0] = data[i].name;
+      likesOldPeople[1] = howMany30PlusFollowed(i);
+    }
+  }
+  console.log(likesOldPeople[0] + ' follows the most people over the age of 30, ' + likesOldPeople[1] + ' of them.');
+}
+
+// TO DO add ability to recognize tie cases
+
+function followsButIsntFollowed(entry) {
+  for (var entry in data) {
+    for (var i = 0; i < data[entry].follows.length; i++) {
+      var followerId = data[entry].follows[i];
+      var notAFollower = false;
+      for (var j = 0; j < data[followerId].follows.length; j++) {
+        if (entry === data[followerId].follows[j]) {
+          notAFollower = true;
+          break;
+        }
+      }
+      if (!notAFollower) {
+        console.log(convertIdtoName(entry) + ' follows ' + convertIdtoName(followerId) + " but isn't followed by them");
+      }
+    }
+  }
+}
+
+
+function followersFollowers() {
+  for (var entry in data) {
+    var sumOfSecondaryFollowers = 0;
+    for (var i = 0; i < data[entry].follows.length; i++) {
+      var followerId = data[entry].follows[i];
+      sumOfSecondaryFollowers += data[followerId].follows.length;
+    }
+    var reach = sumOfSecondaryFollowers + data[entry].follows.length;
+    console.log(convertIdtoName(entry) + ' has a reach of ' +  reach + ' followers');
+  }
+}
+
+
+
+
 // List everyone and for each of them, list the names of who they follow and who follows them
-// followsAndFollowers(); //IT WORKS
+followsAndFollowers(); //IT WORKS
+console.log('------- \n');
 
 // Identify who follows the most people
-// hasFollowsTheMost(); //IT WORKS
+hasFollowsTheMost(); //IT WORKS, doesnt show ties
+console.log('------- \n');
 
 // Identify who has the most followers
-// isFollowedTheMost(); //IT WORKS
+isFollowedTheMost(); //IT WORKS doesn't show ties
+console.log('------- \n');
 
 // Identify who has the most followers over 30
-// mostOldFollowers(); //works
+mostOldFollowers(); //works doesnt show ties
+console.log('------- \n');
 
 // Identify who follows the most people over 30
+followsMostOldPeople(); //WORKS doesnt show ties
+console.log('------- \n');
+
 // List those who follow someone that doesn't follow them back
+followsButIsntFollowed(); //WORKS
+console.log('------- \n');
+
 // List everyone and their reach (sum of # of followers and # of followers of followers)
+followersFollowers(); // WORKS
